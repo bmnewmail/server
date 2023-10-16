ebe4a9def9de190eca7c4a30de862e7adf1a90d9
bmnewmail <148151660+bmnewmail@users.noreply.github.com>
2023-10-16 23:36:43 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-ckmpqrqv7m0s73dhc6gg-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "vRyxclqo9pxYREKiZ2zMZmKkr5YhnzAH",
+    DB: "boommaliboo",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
